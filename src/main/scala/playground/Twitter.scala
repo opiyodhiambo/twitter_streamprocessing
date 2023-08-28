4a970e2d1b3d32724abdc2a398ab10a932d60ccd
opiyodhiambo <rnldlinus@gmail.com>
2023-08-28 12:16:09 +0300

    created Author, Hastag and Tweet case classes

--- src/main/scala/playground/Twitter.scala
+++ src/main/scala/playground/Twitter.scala
@@ -2,3 +2,17 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.scaladsl._
 
+final case class Author(handle: String)
+final case class Hashtag(name: String)
+
+final case class Tweet(account: Author, timestamp: Long, body: String) {
+  def hashtags: Set[Hashtag] = {
+    body
+      .split(" ")
+      .collect {
+        case t if t.startsWith("#") =>
+          Hashtag(t.replaceAll("[^#\\w]", ""))
+      }
+      .toSet
+  }
+}
