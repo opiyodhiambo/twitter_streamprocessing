729b878c444928ef42c24d5a60173e05f9dccb79
opiyodhiambo <rnldlinus@gmail.com>
2023-08-28 13:14:21 +0300

    configured the ActorSystem to terminate after processing the stream

--- build.sbt
+++ build.sbt
@@ -12,5 +12,7 @@ resolvers += "Akka library repository".at("https://repo.akka.io/maven")
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion cross CrossVersion.for3Use2_13,
   "com.typesafe.akka" %% "akka-stream" % akkaVersion cross CrossVersion.for3Use2_13,
-  "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test cross CrossVersion.for3Use2_13
+  "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test cross CrossVersion.for3Use2_13,
+  "ch.qos.logback" % "logback-classic" % "1.2.6",
+  "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.14.1"
 )
--- src/main/scala/playground/Twitter.scala
+++ src/main/scala/playground/Twitter.scala
@@ -1,6 +1,8 @@
-import akka.NotUsed
+import akka.{NotUsed, Done}
 import akka.actor.ActorSystem
 import akka.stream.scaladsl._
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.Future
 
 final case class Author(handle: String)
 final case class Hashtag(name: String)
@@ -18,34 +20,45 @@ final case class Tweet(account: Author, timestamp: Long, body: String) {
 }
 val rugbyTag = Hashtag("#RWC2023")
 
-val tweets: Source[Tweet, NotUsed] = Source(
-  List(
-    Tweet(
-      Author("PlanetRugby"),
-      System.currentTimeMillis,
-      "The warm-ups are done. Bring on the Rugby World Cup! #RWC2023"
-    ),
-    Tweet(
-      Author("RugbyPass"),
-      System.currentTimeMillis,
-      "Fiji make their Twickenham entrance just weeks out from potentially becoming everyone's favorite second team at the Rugby World Cup... #FijiRugby #ENGvFIJ #SummerNationsSeries #RWC2023"
-    ),
-    Tweet(
-      Author("rugbyworldcup"),
-      System.currentTimeMillis,
-      "Ice in the veins. The kick at RWC 2015 against England that secured Dan Biggar's place in Welsh rugby folklore #RWC2023"
-    ),
-    Tweet(
-      Author("AllBlacks"),
-      System.currentTimeMillis,
-      "The commentary on this try couldn't be more perfect #RWC2023"
-      ),
-    Tweet(
-      Author("SVNSeries"),
-      System.currentTimeMillis,
-      "@CheslinKolbe was born to skip past defenders! #HSBCSVNS #RWC2023"
-      )
-  )
-)
+object main extends App {
 
-implicit val system: ActorSystem = ActorSystem("RWC")
+  val tweets: Source[Tweet, NotUsed] = Source(
+    List(
+      Tweet(
+        Author("PlanetRugby"),
+        System.currentTimeMillis,
+        "The warm-ups are done. Bring on the Rugby World Cup! #RWC2023"
+      ),
+      Tweet(
+        Author("RugbyPass"),
+        System.currentTimeMillis,
+        "Fiji make their Twickenham entrance just weeks out from potentially becoming everyone's favorite second team at the Rugby World Cup... #FijiRugby #ENGvFIJ #SummerNationsSeries #RWC2023"
+      ),
+      Tweet(
+        Author("rugbyworldcup"),
+        System.currentTimeMillis,
+        "Ice in the veins. The kick at RWC 2015 against England that secured Dan Biggar's place in Welsh rugby folklore #RWC2023"
+      ),
+      Tweet(
+        Author("AllBlacks"),
+        System.currentTimeMillis,
+        "The commentary on this try couldn't be more perfect #RWC2023"
+      ),
+      Tweet(
+        Author("SVNSeries"),
+        System.currentTimeMillis,
+        "@CheslinKolbe was born to skip past defenders! #HSBCSVNS #RWC2023"
+      )
+    )
+  )
+
+  implicit val system: ActorSystem = ActorSystem("RWC")
+
+  val done: Future[Done] = tweets
+    .filterNot(_.hashtags.contains(rugbyTag))
+    .runWith(Sink.foreach(println))
+
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+  done.onComplete(_ => system.terminate())
+
+}
